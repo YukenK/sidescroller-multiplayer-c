14d1b805039d9946af87edcf835f7c4cb53e9a42
Jason Eagleston <jason@yuken.xyz>
2022-03-28 02:32:23 -0400

    Initial commit.

---
+++ src/main.c
@@ -0,0 +1,174 @@
+#include <enet/enet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+
+#define COMPONENT_DEFAULT_SIZE 1024
+#define ENTITIY_DEFAULT_SIZE 256
+#define PEER_DEFAULT_SIZE 32;
+
+enum ComponentType {
+	NO_COMP,
+};
+enum EntityType {
+	NO_PEER_ENTITY,
+	PEER_ENTITY,
+}
+typedef struct {
+	ComponentType type;
+	void* comp;
+	void* deinit;
+	int has_deinit_function;
+} ComponentContainer;
+typedef struct {
+	EntityType type;
+	uint32_t id;
+} Entity;
+typedef struct {
+	EntityType type;
+	uint32_t id;
+	uint32_t peer_id;
+} PeerEntity;
+typedef struct {
+	Vector components;
+	Vector entities;
+	Vector peers;
+} GameState;
+
+int init_state(GameState* game_state) {
+	init_vector(&game_state->components, COMPONENT_DEFAULT_SIZE, sizeof(ComponentContainer));
+	init_vector(&game_state->entities, ENTITY_DEFAULT_SIZE, sizeof(PeerEntity));
+	init_vector(&game_state->peers, PEER_DEFAULT_SIZE, sizeof(ENetPeer*));
+	for (int i = 0; i < COMPONENT_DEFAULT_SIZE; i++) {
+		ComponentContainer* comp = (ComponentContainer*)vec_get(&game_state->components, i);
+		comp->comp = NULL;
+		comp->deinit = NULL;
+		comp->has_deinit_function = 0;
+		comp->type = NO_COMP;
+	}
+	for (int i = 0; i < ENTITY_DEFAULT_SIZE; i++) {
+		Entity* entity = (Entity*)vec_get(&game_state->entities, i);
+		entity->id = i; // Entity IDs are sequential by default, or set to peer ID. 
+		entity->type = NO_PEER_ENTITY;
+	}
+}
+void deinit_state(GameState* game_state, ENetHost* server) {
+	for (int i = 0; i < game_state->components->size; i++) {
+		ComponentContainer* comp = (ComponentContainer*)vec_get(&game_state->components, i);
+		if (comp->has_deinit_function) {
+			comp->deinit(comp);
+		}
+	}
+	int peers_left;
+	for (peers_left = 0; peers_left < game_state->peers->size; peers_Left++) {
+		Peer* peer = *(Peer**)vec_get(&game_state->peers, peers_left);
+		enet_peer_disconnect(peer, 0);
+		PeerEntity* entity = NULL;
+		for (int i = 0; i < game_state->entities->size; i++) {
+			entity = (PeerEntity*)vec_get(&game_state->entities, i);
+			if (entity->ComponentType == PEER_ENTITY && entity->peer_id == peer->id) {
+				entity->ComponentType = NO_PEER;
+				entity->peer_id = 0;
+				entity->id = 0;
+				vec_remove(&game_state->entities, i);
+				break;
+			}
+			entity = NULL;
+		}
+	}
+	ENetEvent event;
+	while (enet_host_service(host, &event, 5000)) {
+		switch (event.type) {
+			case ENET_EVENT_TYPE_RECEIVE:
+				enet_packet_destroy(event.packet);
+				break;
+			case ENET_EVENT_TYPE_DISCONNECT:
+				peers_left--;
+			default:
+				break;
+		}
+	}
+	if (peers_left) {
+		for (int i = 0; i < game_state->peers->size; i++) {
+			Peer* peer = (Peer*)vec_get(&game_state->peers, i);
+			enet_peer_reset(peer);
+		}
+	}
+	deinit_vector(&game_state->components);
+	deinit_vector(&game_state->entities);
+	deinit_vector(&game_state->peers);
+}
+void handle_packet(GameState* game_state, ENetPeer* peer, ENetPacket* packet) {
+	uint16_t packet_type;
+	if (packet->dataLength == 0) {
+		fprintf(stderr, "Zero length packet received from peer: %s\n", peer->id);
+	}
+	else if (packet->dataLength == 1) {
+		packet_type = get_u8(packet->data, 0);
+	} else if (packet->dataLength > 1) {
+		uint16_t packet_type = get_u16(packet->data, 0);
+	}
+}
+int main(int argc, char** argv) {
+	ENetAddress address;
+	ENetHost* server;
+	unsigned int port = 5515;
+	char* host_address = "0.0.0.0";
+	GameState game_state;
+	init_state(&game_state);
+
+
+	if (enet_initialize() != 0) {
+		fprintf(stderr, "An error occured while initializing enet.\n");
+		return EXIT_FAILURE;
+	}
+	atexit(enet_deinitialize);
+
+	for (int i = 1; i < argc; i++) {
+		char* arg = *(argv + i);
+		if (strcmp(arg, "-p") == 0 || strcmp(arg, "--port") == 0) {
+			i++;
+			port = strtol(*(argv + i), NULL, 10);
+			break;
+		} else if (strcmp(arg, "--host") == 0) {
+			i++;
+			host_address = *(argv + i);
+			enet_address_set_host(&address, host_address);
+			break;
+		}
+		fprintf(stderr, "Unknown argument found: %s\n", arg);
+	}
+
+	printf("Listening on: %s:%d\n", host_address, port);
+
+	int quit = 0;
+
+	ENetEvent event;
+	while (!quit) {
+		while (enet_host_service(server, &event, 0) > 0) {
+			switch (event.type) {
+				case ENET_EVENT_TYPE_CONNECT:
+					printf("Client joined: %x:%u\n", event.peer->address.host, event.peer->address.port);
+					break;
+				case ENET_EVENT_TYPE_RECEIVE:
+					handle_packet(&game_state, event.peer, event.packet);
+					enet_packet_destroy(event.packet);
+					break;
+				case ENET_EVENT_TYPE_DISCONNECT:
+					printf("Client disconnected: %s\n", event.peer->data);
+					event.peer->data = NULL;
+					break;
+				default:
+					break;
+
+			}
+		}
+	}
+	
+	enet_host_flush(host);
+
+	deinit_state(&game_state, host);
+
+	return 0;
+}
---
+++ src/vector.h
@@ -0,0 +1,66 @@
+#ifndef YUKARRAY
+#define YUKARRAY
+typedef struct {
+	unsigned int cursor;
+	void* data;
+	int* filled;
+	unsigned int max_size;
+	unsigned int size;
+	size_t type_size;
+} Vector;
+void deinit_vector(Vector* vec) {
+	free(vec->data);
+	free(vec->filled);
+}
+void init_vector(Vector* vec, unsigned int size, size_t type_size) {
+	vec->cursor = 0;
+	vec->data = malloc(type_size * size);
+	vec->filled = calloc(size, sizeof(unsigned int));
+	vec->max_size = size;
+	vec->size = 0;
+	vec->type_size = type_size;
+}
+void vec_resize(Vector* vec, unsigned int size) {
+	void* data = malloc(vec->type_size * size);
+	int* filled = calloc(size, sizeof(unsigned int);
+	memmove(data, vec->data, vec->size);
+	memmove(filled, data->filled, vec->size);
+	free(vec->data);
+	free(vec->filled);
+	vec->data = data;
+	vec->max_size = size;
+}
+void* vec_get(Vector* vec, pos) {
+	if (*(vec->filled + pos) == 1) {
+		return vec->data + pos;
+	}
+	return NULL;
+}
+void* vec_pop(Vector* vec) {
+	if (*(vec->filled + (vec->cursor - 1)) == 1) {
+		void* data = malloc(vec->type_size);
+		memmove(data, vec->data + vec->cursor, vec->type_size);
+		*(vec->filled + vec->cursor) = 0;
+		return data;
+	}
+	return NULL;
+}
+void vec_push(Vector* vec, void* data) {
+	if (vec->size == vec->max_size) {
+		vec_resize(vec, vec->size * 1.5);
+	}
+	int cursor_loop = 1;
+	while (cursor_loop) { // We will eventually find or create a free slot, or run out of memory; no need for many checks here...?
+		if (vec->cursor == vec->max_size) {
+			vec_resize(vec, vec->size * 1.5);
+			cursor_loop = 0;
+		} else if (*(vec->filled + vec->cursor) == 0) {
+			*(vec->filled + vec->cursor) = 1;
+			memmove(vec->data + vec->cursor, data, vec->type_size);
+			cursor_loop = 0;
+		}
+		vec->cursor++;
+	}
+}
+void* vec_remove(
+#endif
